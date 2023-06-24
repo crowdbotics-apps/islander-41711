a33f781ee5d17bf3d249cd8e0eaac2b688082f4f
Crowdbotics <team@crowdbotics.com>
2023-06-24 02:13:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "islander-41711",
+  url: "https://islander-41711.botics.co",
+  api: "https://islander-41711.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
